5ace30ea20b997e74dbce2cf7d4b2cb3c2e45c31
gulivero1773 <gulivero1773@gmail.com>
2020-04-08 19:17:41 +0300

    It work https://medium.com/@martinlasek/tutorial-how-to-build-bearer-auth-8ae3f80b9522

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,32 +1,34 @@
-import FluentSQLite
 import Vapor
+import Leaf
+import FluentSQLite
+import Authentication
 
 /// Called before your application initializes.
-public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
-    try services.register(FluentSQLiteProvider())
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+) throws {
 
-    // Register routes to the router
+    /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
 
-    // Register middleware
-    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
-    services.register(middlewares)
+    let leafProvider = LeafProvider()
+    try services.register(leafProvider)
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    try services.register(FluentSQLiteProvider())
 
-    // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    try databases.add(database: SQLiteDatabase(storage: .memory), as: .sqlite)
     services.register(databases)
 
-    // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: User.self, database: DatabaseIdentifier<User.Database>.sqlite)
+    migrations.add(model: Token.self, database: DatabaseIdentifier<Token.Database>.sqlite)
     services.register(migrations)
+    
+    try services.register(AuthenticationProvider())
 }
